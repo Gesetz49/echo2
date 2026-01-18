358eb4d602b098cba73166982658333c56c26631
Андрей <dmtheory1818@gmail.com>
2026-01-18 10:34:05 +0300

    Make app non-interactive + update deploy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package org.example
 
 fun main() {
-    print("Введите первое число: ")
+/*    print("Введите первое число: ")
     val first = readLine()?.toDoubleOrNull()
 
     print("Введите второе число: ")
@@ -13,5 +13,22 @@ fun main() {
     }
 
     val sum = first + second
-    println("Сумма двух чисел: $sum")
+    println("Сумма двух чисел: $sum")*/
+
+    fun main(args: Array<String>) {
+        if (args.size < 2) {
+            println("Usage: java -jar app.jar <number1> <number2>")
+            return
+        }
+
+        val num1 = args[0].toDoubleOrNull()
+        val num2 = args[1].toDoubleOrNull()
+
+        if (num1 == null || num2 == null) {
+            println("Error: both arguments must be numbers")
+            return
+        }
+
+        println("Сумма двух чисел: ${num1 + num2}")
+    }
 }
